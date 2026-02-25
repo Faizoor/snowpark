4192c1466018f2504954935e45126faa02276c03
Faizoor <tafyzoor123@gmail.com>
2026-02-26 00:31:17 +0530

    added functional checks and written the results to csv

--- Framework/lib/checks.py
+++ Framework/lib/checks.py
@@ -1,34 +1,58 @@
-# This Module contains SQL generation for Testing Framework
+# This Module contains SQL generation for Testing Framework checks
 #   Function takes table names and rules as parameter and returns a generated SQL
 #   
 
+def null_check(object_name,column_name):
+    try:
+        Full_table_name=object_name.strip()
+        parts = Full_table_name.split('.')
+
+        database, schema, table_name = [p.upper() for p in parts]
+
+        condition=" OR ".join([f"{col} IS NULL" for col in column_name])
 
 
-def null_check(table_name,rule):
-    pass
+        generated_sql=f"""
+                    SELECT count(*) as result
+                    FROM {database}.{schema}.{table_name}
+                    WHERE {condition};
+                    """
+        return generated_sql.strip()
+    except Exception as e:
+        raise ValueError(f"Error building null_check: {str(e)}")
 
 
-def unique_check():
+def table_exists(object_name):   
+    """Check if a table exists in Snowflake"""
+    try:
+        Full_table_name=object_name.strip()
+        parts = Full_table_name.split('.')
+
+        database, schema, table_name = [p.upper() for p in parts]
+
+        generated_sql=f"""
+                    SELECT count(*) as result
+                    FROM {database}.INFORMATION_SCHEMA.TABLES
+                    WHERE TABLE_SCHEMA='{schema}'
+                    AND TABLE_NAME='{table_name}';
+                    """
+        return generated_sql.strip()
+    except Exception as e:
+        raise ValueError(f"Error building table_exists check: {str(e)}")
+
+def duplicate_check():
     pass
 
 
 def stage_exists(object_name):
-    return f"{object_name}"
+    pass
 
-def table_exists(object_name):   
-    Full_table_name=object_name.strip()
-    database=Full_table_name.split('.')[0].upper()
-    schema=Full_table_name.split('.')[1].upper()
-    table_name=Full_table_name.split('.')[2].upper()
-    generated_sql=f"""
-                SELECT count(*) as result
-                FROM {database}.INFORMATION_SCHEMA.TABLES
-                WHERE TABLE_SCHEMA='{schema}'
-                AND TABLE_NAME='{table_name}';
-                """
-    return generated_sql.strip()
+
+
 sanity_registry= {
     "stage_exists" : stage_exists,
-    "table_exists" : table_exists
+    "table_exists" : table_exists,
+    "null_check" : null_check,
+    "duplicate_check" : duplicate_check
 
 }
--- Framework/main.py
+++ Framework/main.py
@@ -7,27 +7,38 @@ from dotenv import load_dotenv
 import os 
 import logging 
 from lib import checks
+import pandas as pd
+import csv
 
 
 class SnowparkValidationRunner():
     """ 
-            Description about the class
+        Main class for running Sanity and Functional checks for Data Quality Testing Framework.
+            - sanity_checks: checks to validate the existence of objects and basic checks before running DQ validations
+            - functional_checks: checks to validate the data quality based on the rules defined in the config file
+            parameters:
+                session: Snowflake session object
+                yaml_path: path to the yaml config file 
     """
 
     def __init__(self,session,yaml_path):
+        """
+            Initialize the class with session and yaml path, load the config.
+        """
+
         self.session= session
         self.yaml_path = yaml_path
         self.config = self.config_loader()
-        #print(self.config.get('tables'))
+        #logger.info(self.config.get('tables'))
         self.application = self.config.get("application")
         self.database = self.config.get("database")
         self.schema = self.config.get("schema")
         
     
         #logging the database, schema and applications details
-        print(f"    Applicatoin: {self.application}")
-        print(f"    Database:    {self.database}")
-        print(f"    Schema:      {self.schema}")
+        logger.info(f"    Applicatoin: {self.application}")
+        logger.info(f"    Database:    {self.database}")
+        logger.info(f"    Schema:      {self.schema}")
 
 
     def execute(self):
@@ -36,28 +47,31 @@ class SnowparkValidationRunner():
         """
 
         #Run sanity checks
-        #for checks in self.config.get("checks")['sanity_checks']:
-        #    print(checks)
-        self.sanity_checks()
+        sanity_check_result=self.sanity_checks()    
+        #logger.info(f"Sanity check results: {sanity_check_result}")
 
         #Data Quality checks
-        tables = self.config.get("rules")
+        functional_check_result=self.functional_checks()
 
-        #for table in tables:
-        #    table_name=tables.get("name")
-        #    enabled=tables.get("enabled",True)
-        #    if not enabled:
-        #        print(f"DQ check is not enabled for this table: , {self._build_table_name(table_name)}")
-        #        continue
+        #checks summary
+        sanity_summary=self._get_summary(sanity_check_result)
+        logger.info(f"Sanity checks summary: {sanity_summary}")
+        functional_summary=self._get_summary(functional_check_result)
+        logger.info(f"Functional checks summary: {functional_summary}")
+    
+        # return results
+        return {
+            "sanity":sanity_check_result,
+            "Functional":functional_check_result,
+           
 
-        #    qualified_table_name = self._build_table_name(table_name)
-        #    print(f"running checks for the table")
+        }
 
 
     def sanity_checks(self):
 
         """
-        Execute sanity checks before DQ validations
+        Execute sanity checks 
         
         """
         sanity_conf=self.config.get("sanity_checks")
@@ -66,8 +80,8 @@ class SnowparkValidationRunner():
 
         if not sanity_conf:
             return f"No sanity checks to perform"
-        print(f"Running sanity checks")
-        print(sanity_conf)
+        logger.info(f"Running sanity checks")
+        #logger.info(sanity_conf)
         for rule in sanity_conf.get('rules'):
             rule_id = rule.get('rule_id')
             name = rule.get('name') 
@@ -78,29 +92,35 @@ class SnowparkValidationRunner():
             error_message=None
             status=None
 
-            print(rule_id)
-            print(name)
-            print(object_name)
-            print(rule_type)
+            #logger.info("rule_id: %s", rule_id)
+            #logger.info("name: %s", name)
+            #logger.info("object_name: %s", object_name)
+            #logger.info("rule_type: %s", rule_type)
+            #logger.info(object_type)
 
-            print(object_type)
+            logger.info(f"Executing sanity check: rule_id - {rule_id} - on object: {object_name} with rule type: {rule_type}")  
 
             if rule_type not in checks.sanity_registry:
                 raise ValueError(f"Unsupported sanity check")
-            #print(checks.sanity_registry['stage_exists'])
+            #logger.info(checks.sanity_registry['stage_exists'])
             check_function = checks.sanity_registry[rule_type]
             try:
                 sql = check_function(object_name)
-                print(sql)
+                #logger.info(sql)
 
                 result = self.session.sql(sql).collect()
-                print(result)
+                #logger.info(result)
                 
                 value = result[0]['RESULT']
                 status="PASS" if value>0 else "FAIL"
+                if status=="PASS":
+                    logger.info(f"Sanity check: rule_id - {rule_id} - on object: {object_name} with rule type: {rule_type} has PASSED")
+                else:
+                    logger.info(f"Sanity check: rule_id - {rule_id} - on object: {object_name} with rule type: {rule_type} has FAILED")
             except Exception as e:  
                 status = "ERROR"
-                error_message=str(e)
+                error_message=getattr(e, 'message', str(e))
+                logger.error(f"Error executing sanity check with {rule_id} on object - {object_name}: {error_message}")
 
             sanity_results.append(
                     {
@@ -114,9 +134,101 @@ class SnowparkValidationRunner():
                 )
         return sanity_results
 
-                
-    def _build_table_name(self,table_name):
-        return f"{self.database.upper()}.{self.schema.upper()}.{table_name.upper()}"
+    def functional_checks(self):
+        """
+        Execute functional checks for DQ validations
+        """
+
+        functional_conf=self.config.get("functional_checks")
+
+        functional_results=[]
+
+        error_message=None
+        status=None
+
+        if not functional_conf:
+            return f"No functional checks to perform"
+        
+        logger.info(f"Running functional checks")
+
+        for rule in functional_conf.get('rules'):
+            rule_id = rule.get('rule_id')
+            name=rule.get('name')
+            rule_type=rule.get('rule_type')
+            object=rule.get('object')
+            threshold=rule.get('threshold')
+
+            #logger.info(f"rule_id: {rule_id}")
+            #logger.info(f"name: {name}")
+            #logger.info(f"rule_type: {rule_type}")
+            #logger.info(f"object: {object}")
+
+            print("Hi")
+            if rule_type not in checks.sanity_registry:
+                raise ValueError(f"Unsupported sanity check")
+
+            check_function = checks.sanity_registry[rule_type]
+            try:
+                for obj in object:
+                    object_name=obj.get('name')
+                    columns=obj.get('columns')
+                    #print(obj['name'])
+                    #logger.info(f"object_name: {object_name}")
+                    #logger.info(f"columns: {columns}")
+
+                    #print(check_function.__name__)
+                    #print(checks.null_check(object_name,columns))
+                    sql = check_function(object_name,columns)
+                    #logger.info(sql)
+                    result = self.session.sql(sql).collect()
+                    value = result[0]['RESULT']
+
+                    status = "PASS" if value==threshold else "FAIL"
+                    if status=="PASS":
+                        logger.info(f"Functional check: rule_id - {rule_id} - on object: {object_name} with rule type: {rule_type} has PASSED")
+                    else:
+                        logger.info(f"Functional check: rule_id - {rule_id} - on object: {object_name} with rule type: {rule_type} has FAILED")
+
+                    functional_results.append(
+                        {
+                            "rule_id": rule_id,
+                            "name": name,
+                            "object": object_name,
+                            "rule_type": rule_type,
+                            "status": status,
+                            "error_message": None}
+                    )
+
+            except Exception as e:  
+                status = "ERROR"
+                error_message=getattr(e, 'message', str(e))
+                logger.error(f"Error executing functional check with {rule_id} on object - {object_name}: {error_message}")
+                functional_results.append(
+                        {
+                            "rule_id": rule_id,
+                            "name": name,
+                            "object": object_name,
+                            "rule_type": rule_type,
+                            "status": status,
+                            "error_message": error_message}
+                    )
+        return functional_results
+
+    def _get_summary(self,results):
+        """
+        Return summary of the checks with total, passed, failed and error counts
+        """
+        total_checks = len(results)
+        passed_checks = len([r for r in results if r['status'] == 'PASS'])
+        failed_checks = len([r for r in results if r['status'] == 'FAIL'])
+        error_checks = len([r for r in results if r['status'] == 'ERROR'])
+        return {
+            "total": total_checks,
+            "passed": passed_checks,
+            "failed": failed_checks,
+            "error": error_checks
+        }
+        
     
     def config_loader(self):
         """
@@ -126,14 +238,42 @@ class SnowparkValidationRunner():
             return yaml.safe_load(conf)
 
 
+    def write_results_csv(self,results):
+        """
+        Write the results to a csv file in the local directory
+        """
+        rows=[]
 
+        for category in ["sanity","Functional"]:
+            rows.extend(
+                [{'category':category,**item}for item in results[category]]
+            )
 
+        df=pd.DataFrame(rows)
+        df.to_csv("test_result.csv",index=False)
 
+        logger.info(f"Results written to test_result.csv")
+       
 
+        
+        
 
 if __name__ == "__main__":
+    """
+    Main function to execute the testing framework
+    """
+
     load_dotenv()
     yaml_path = "Framework/config.yaml"
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s | %(levelname)s | %(message)s"
+    )
+    logger=logging.getLogger(("snowpark_testing"))
+
+    logging.getLogger("snowflake").setLevel(logging.WARNING)
+    logging.getLogger("snowflake.snowpark").setLevel(logging.WARNING)
 
     connection_parameters = {
     "account": os.environ["SNOWFLAKE_ACCOUNT"],
@@ -149,7 +289,11 @@ if __name__ == "__main__":
     #create Framework Object 
         snowpark_testing = SnowparkValidationRunner( session, yaml_path  )
         results=snowpark_testing.execute()
-        #print(f"results: {results}")
+        #logger.info(f"results: {results}")
+
+    # Export the output as csv format 
+        snowpark_testing.write_results_csv(results)
+
 
     except Exception as e :
-        print(f"Script aborted due to this error {type(e)}:  {e}")
+        logger.info(f"Script aborted due to this error {type(e)}:  {e}")
